6cc171555e87d73d7b8e0e8db26faad6f85b8334
铭记mingji <109053797+mingjigege@users.noreply.github.com>
2023-05-28 11:30:22 +0800

    Update Title.js

--- Title.js
+++ Title.js
@@ -18,10 +18,6 @@ const defaultplayer = {  //玩家数据文件
     "title": [config.get("DefaultTitle"), '测试', 'a', '测试'],
     "use": config.get("DefaultTitle")
 };
-/*const defaultshop = {  //商店数据文件
-    "title": tname,
-    "money": money
-};*/
 
 mc.listen("onServerStarted", () => {
     let cmds = mc.newCommand("titleshop", "§e称号管理", PermType.Any);
@@ -56,7 +52,7 @@ function main(pl) {
                 shop(pl);
                 break;
             case 2:
-                pl.tell("还没写")
+                admin(pl);
                 break;
             default:
                 break;
@@ -95,39 +91,46 @@ function titeplayer(pl) {
     });
 }
 function shop(pl) {
+    let tname = "默认名字";
+    let money = "默认金币";
     let fm = mc.newSimpleForm();
     let players = new KVDatabase("./plugins/Title/playerdb");
-    let db = players.get(shop);
     let player = players.get(pl.uuid);
-    log(db)
-
+    let defaultshop = {  // 商店数据文件
+        "title": tname,
+        "money": 1
+    };
+    let db = players.get('shop'); // 修复获取商店数据的问题
     if (!db) {
-        players.set('shop', {});
+        players.set('shop', defaultshop);
+        db = defaultshop; // 修复第一次创建商店数据时的数据返回问题
     }
-    if (!db.title) {
-        pl.tell("称号商店无数据,快让服主添加几个吧")
-        return
+    if (typeof db.title === 'undefined') { // 优化判断 db.title 是否存在
+        pl.tell("称号商店无数据,快让服主添加几个吧");
+        return;
     }
+    log(db);
     fm.setTitle("称号商店");
     fm.setContent("请选购");
 
-    db.title.sort();
     player.title.sort();
+    fm.addButton(db.title + " §e价格§d:§r " + db.money);
 
-    for (let i = 0; i < db.title.length; i++) {
-        fm.addButton(db.title[i]);
-    }
     pl.sendForm(fm, (pl, arg) => {
         if (player.title.includes(db.title[arg])) {
             pl.tell("该称号已拥有,请勿重复购买");
             return;
         }
         if (db.title[arg] != undefined) {
-            let money = money.get(pl.xuid)
-            money.reduce(pl.xuid, money)
-            //log(db.title[arg].money)
+            let money = pl.getMoney()
+            log(db[arg].money)
+            log(money)
             if (money > db.title[arg].money) {
-                pl.tell("购买成功,以获得" + db.title[arg] + "称号\n消耗金币数量:" + db.title[arg].money)
+                pl.reduceMoney(money)
+                pl.tell("购买成功,以获得" + db[arg] + "称号\n消耗金币数量:" + db[arg].tmoney)
+            }
+            else {
+                pl.tell("购买失败,j")
             }
         }
         else {
@@ -137,6 +140,43 @@ function shop(pl) {
         return;
     }
     );
+}
+function admin(pl) {
+    let fm = mc.newSimpleForm();
+    let players = new KVDatabase("./plugins/Title/playerdb");
+    let db = players.get(pl.uuid);
+
+    fm.setTitle("管理玩家称号");
+    fm.setContent("欢迎管理员" + pl.realName);
+    fm.addButton("商店新增称号");
+    fm.addButton("商店删除称号");
+    fm.addButton("管理所有玩家");
+
+    pl.sendForm(fm, (pl, id) => {
+        switch (id) {
+            case 0:
+                add(pl);
+                break;
+            case 1:
+                remove(pl);
+                break;
+            case 2:
+                op(pl);
+                break;
+            default:
+                break;
+        }
+    });
+
+}
+function add(pl) {
+
+}
+function remove(pl) {
+
+}
+function op(pl) {
+
 }
 mc.listen("onJoin", function (pl) {
     let players = new KVDatabase("./plugins/Title/playerdb");
